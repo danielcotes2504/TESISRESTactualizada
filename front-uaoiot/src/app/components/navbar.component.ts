0a64db6290d89f866db28c32cb1b36fc9fc29a92
Diana Urrutia <valentina.us@hotmail.com>
2021-09-01 11:11:02 -0500

    dispositivos
    se hacen algunos cmabios en los estilos de los dispositivos

--- front-uaoiot/src/app/components/navbar.component.ts
+++ front-uaoiot/src/app/components/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { UserLoginService } from '../services/userLogin.service';
