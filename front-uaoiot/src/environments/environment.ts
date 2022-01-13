1299bd7f3ae1ed05f08d1ea71ce4de537a06160f
danielcotes2504 <danielenrique2000@hotmail.com>
2022-01-13 11:20:03 -0500

    Cambio server centos prod
    Se cambió las IP

--- front-uaoiot/src/environments/environment.ts
+++ front-uaoiot/src/environments/environment.ts
@@ -5,9 +5,9 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3000/',
-  baseUrlSocket: 'http://localhost:5000/',
-  restUrl: 'http://localhost:8000/',
+  baseUrl: 'http://inteocav2.uao.edu.co:3000/',
+  baseUrlSocket: 'http://inteocav2.uao.edu.co:5000/',
+  restUrl: 'http://inteocav2.uao.edu.co:8000/',
 };
 
 // export const environment = {
--- server/src/enviroment.js
+++ server/src/enviroment.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
 
-const IP_ADDRESS = "http://localhost:"
+const IP_ADDRESS = "http://inteocav2.uao.edu.co:"
 
 exports.IP_ADDRESS = IP_ADDRESS;
