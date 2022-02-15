5c24866975609bd686e1f58850494f3c570eb09a
danielcotes2504 <danielenrique2000@hotmail.com>
2022-02-15 13:23:18 -0500

    AdminComponente
    Se arregló el bug de que un usuario al cambiar la ruta podía acceder facilmente  a todo el entorno de administración

--- front-uaoiot/src/app/components/admin.component.ts
+++ front-uaoiot/src/app/components/admin.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { AuthService } from '../services/auth.service';
+import { UserLoginService } from '../services/userLogin.service';
 import { Router } from '@angular/router';
 
 
@@ -11,11 +12,43 @@ import { Router } from '@angular/router';
     providers: [UserService]
 })
 
-export class AdminComponent {
+export class AdminComponent implements OnInit{
     public users: Array<String>;
-
-    constructor(private _userService: UserService,
-        private auth: AuthService, private router: Router) {
+    public userNameFromLogin;
+    public stringUserName;
+    constructor(
+        public authService: AuthService,
+        public userLoginService: UserLoginService,
+        public router: Router
+    ) {
 
     }
+    ngOnInit() {
+
+        this.authService.getLoggedInName.subscribe(name => this.stringUserName = name);
+        this.authService.getLoggedInUser.subscribe(user => this.userNameFromLogin = user);
+        console.log(this.authService.getLoggedInUser.subscribe(user => this.userNameFromLogin = user))
+        this.getUserName();
+        console.log(this.userNameFromLogin)
+      if(this.userNameFromLogin !== "admin"){
+        this.router.navigate(['/restProjects']);
+      }
+        
+       
+       
+
+    }
+    getUserName() {
+        this.stringUserName = '';
+        if (sessionStorage.getItem('user') == null) {
+            this.stringUserName = 'perfil';
+        } else {
+            const userProfile = JSON.parse(sessionStorage.getItem('user'));
+            this.userNameFromLogin = userProfile.user;
+            this.stringUserName = userProfile.name;
+
+            
+        }
+    }
+
 }
--- front-uaoiot/src/app/components/tutorialAdmin.component.ts
+++ front-uaoiot/src/app/components/tutorialAdmin.component.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { AuthService } from '../services/auth.service';
+import { UserLoginService } from '../services/userLogin.service';
+import { Router } from '@angular/router';
 @Component({
     selector: 'app-tutorial-admin',
     templateUrl: '../views/tutorialAdmin.component.html',
     styleUrls: ['../styles/tutorialAdmin.component.scss']
 })
 
-export class TutorialAdminComponent { }
+export class TutorialAdminComponent implements OnInit {
+    public userNameFromLogin;
+    public stringUserName;
+    constructor(
+        public authService: AuthService,
+        public userLoginService: UserLoginService,
+        public router: Router
+    ) {
+
+    }
+    ngOnInit() {
+
+        this.authService.getLoggedInName.subscribe(name => this.stringUserName = name);
+        this.authService.getLoggedInUser.subscribe(user => this.userNameFromLogin = user);
+        console.log(this.authService.getLoggedInUser.subscribe(user => this.userNameFromLogin = user))
+        this.getUserName();
+        console.log(this.userNameFromLogin)
+      if(this.userNameFromLogin !== "admin"){
+        this.router.navigate(['/restProjects']);
+      }
+        
+       
+       
+
+    }
+    getUserName() {
+        this.stringUserName = '';
+        if (sessionStorage.getItem('user') == null) {
+            this.stringUserName = 'perfil';
+        } else {
+            const userProfile = JSON.parse(sessionStorage.getItem('user'));
+            this.userNameFromLogin = userProfile.user;
+            this.stringUserName = userProfile.name;
+
+            
+        }
+    }
+ }
